a41d7910a464d5d2315f67f46f3f7393eceb9316
Edu58 <edumuriithi58@gmail.com>
2022-02-24 16:12:37 +0300

    first commit: react boiler plate

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseInit = initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID
+});
+
+export const auth =  firebaseInit.auth()
+export default firebaseInit;
